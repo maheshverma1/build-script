aed8e7309bce9ff8186c6a8a9ca71d1bd1d985f7
Mahesh Kumar <maheshverma1509@gmail.com>
2019-12-15 15:37:03 +0530

    first commit

---
+++ build.bat
@@ -0,0 +1,10 @@
+#echo "mvn clean install"
+#mvn clean install
+#$(git rev-parse HEAD)
+
+docker build -t maheshkumarctl/docker-k8s-example:latest .
+
+docker login --username=maheshkumarctl --password=mk@verma1
+
+docker push maheshkumarctl/docker-k8s-example:latest
+
